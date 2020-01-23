de887db21eb5275236d25af1be8c1dcc773ea236
idrisBellahsen <39699663+idrisBellahsen@users.noreply.github.com>
2020-01-23 10:12:15 +0100

    requete

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+
 
 export default function App() {
+  
+  
+  let res;
+  axios.get('https://randomuser.me/api/')
+  .then(function (response) {
+    // handle success
+     res = response;
+
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  
+  
+  
+  
+  
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{res}</Text>
     </View>
   );
 }
