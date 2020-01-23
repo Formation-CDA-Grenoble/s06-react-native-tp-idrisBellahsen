2dd7cfce2322fc2a0e7861400dda4f0b1be6c648
idrisBellahsen <39699663+idrisBellahsen@users.noreply.github.com>
2020-01-23 16:18:48 +0100

    Requete api chien

--- components/PersonList.jsx
+++ components/PersonList.jsx
@@ -1,35 +1,37 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-
+import { View, Image } from 'react-native';
+import axios from 'axios';
 import data from '../data'
 
 export default class PersonList extends Component {
-  state = {
-    persons: []
+   
+    constructor(props) {
+        super(props);
+        this.state = {
+          imageURL: '',
+        }
+      }
+
+  componentDidMount() {
+    axios.get('https://dog.ceo/api/breeds/image/random')
+    .then(response => {
+      this.setState({ imageURL: response.data.message });
+      console.log("didmount");
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 
 
-  render() {
 
 
-    // console.log("2")
-    //  console.log(this.state.persons["results"])
-    console.log("1")
-    let result = data().then(
-        
-    );
-
-    console.log("3")
-    console.log(result)
- 
-    return (
-
-      <View>
-          {/* <Text>
-        {this.state.persons["id"]}</Text>  
-         */}  
-      </View>    
-     
-    )
-  }
+    render() {
+        const { imageURL } = this.state;
+        console.log(imageURL);
+        return (
+            <Image source={{uri: imageURL }}  style={{width: 500, height: 580}}></Image>
+          
+        );
+      }
 }
